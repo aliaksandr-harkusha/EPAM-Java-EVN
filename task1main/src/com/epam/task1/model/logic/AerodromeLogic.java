29c147c2936db16d044dc9b522df8d3703061568
aliaksandr-harkusha <38460516+aliaksandr-harkusha@users.noreply.github.com>
2018-04-22 16:32:53 +0300

    Update AerodromeLogic.java

--- task1main/src/com/epam/task1/model/logic/AerodromeLogic.java
+++ task1main/src/com/epam/task1/model/logic/AerodromeLogic.java
@@ -1,3 +1,5 @@
+//пакет logic - не должен располагаться внутри модели
+//вынеси его рядышком с моделью расположи
 package com.epam.task1.model.logic;
 
 import com.epam.task1.exception.InputLogicException;
@@ -10,6 +12,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class AerodromeLogic {
+    //синглтоны пока использовать не будем
     private static AerodromeLogic instance = new AerodromeLogic();
 
     public static AerodromeLogic getInstance() {
@@ -19,6 +22,8 @@ public class AerodromeLogic {
     public int countTotalCarryingCapacity(Aerodrome aerodrome) {
         int totalCapacity = 0;
         for (int i = 0; i < aerodrome.size(); i++) {
+            //здесь лучше 1 раз перед циклом вызвать aerodrome.getPlanes()  
+            //а потом пройтись по коллекции циклом forEach вместо цикла for
             Plane currentPlane = aerodrome.getPlanes().get(i);
             totalCapacity += currentPlane.getCarryingCapacity();
         }
@@ -62,6 +67,8 @@ public class AerodromeLogic {
 
 
     public void sortOnFlightRange(Aerodrome aerodrome) {
+       //лучше раелизуй компаратор внутри класса Plane
+        // так при компиляции не будет создаваться дополнительный анонимный класс
         Collections.sort(aerodrome.getPlanes(), new Comparator<Plane>() {
             @Override
             public int compare(Plane o1, Plane o2) {
@@ -81,6 +88,7 @@ public class AerodromeLogic {
                 return currentPlane;
             }
         }
+        //перестрой метод так чтобы null не возвращать
         return null;
     }
 
